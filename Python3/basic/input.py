78ef993804d67a0b646681003a9a3b3709f172af
Harish Kumar <harishkumar@Harishs-MacBook-Pro.local>
2019-10-08 14:44:22 +0900

    First Commit

---
+++ Python3/basic/input.py
@@ -0,0 +1,3 @@
+print("What is your Name : ")
+name = input()
+print(f"Hi there, {name}")
---
+++ Python3/basic/truthiness.py
@@ -0,0 +1,8 @@
+animal = input("What is your fav animal : ")
+
+if animal:
+	print(animal, " is my fav too!")
+elif !(animal):
+	print("You should try to answer ")
+else:
+	print("did you forgot to tell your fav animal")
